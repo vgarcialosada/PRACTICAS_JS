e6afc6b828c4ba41be13e30d424ee0a243607330
Victor <vgarcialosada@gmail.com>
2022-08-10 12:26:22 +0200

    .

---
+++ P10/main.js
@@ -0,0 +1,9 @@
+stringTest = "aaaaeeeeeiiiioooouuu";
+stringTest2="Barcelona";
+
+function replaceAforO(str) {
+  return str.replaceAll("a", "o");
+}
+console.log(replaceAforO(stringTest));
+console.log(replaceAforO(stringTest2));
+
