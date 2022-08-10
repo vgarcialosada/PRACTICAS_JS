42a921efe033dbd0a203326d65cc0274ff9285c5
Victor <vgarcialosada@gmail.com>
2022-08-10 12:45:32 +0200

    .

---
+++ P11/main.js
@@ -0,0 +1,12 @@
+
+console.log('***for***');
+for (let index = 0; index < 10; index++) {
+    console.log(index+1+".I ❤️ code");
+}
+
+let counter=0;
+console.log('***while***');
+while (counter<10) {
+    counter++;
+    console.log(counter.toString()+".I ❤️ code");
+}
