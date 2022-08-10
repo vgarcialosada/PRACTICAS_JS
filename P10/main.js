16a400d948c19f5c110b8e3a678ac560b6af45f0
Victor <vgarcialosada@gmail.com>
2022-08-10 12:36:00 +0200

    .

--- P10/main.js
+++ P10/main.js
@@ -1,5 +1,7 @@
+/*E1*/
+console.log("*****E1******");
 stringTest = "aaaaeeeeeiiiioooouuu";
-stringTest2="Barcelona";
+stringTest2 = "Barcelona";
 
 function replaceAforO(str) {
   return str.replaceAll("a", "o");
@@ -7,3 +9,29 @@ function replaceAforO(str) {
 console.log(replaceAforO(stringTest));
 console.log(replaceAforO(stringTest2));
 
+/*E2*/
+console.log("*****E2******");
+
+stringTest = "academia";
+stringTest2 = "escuela";
+function startsWithAca(str) {
+  stringBegins = str[0] + str[1] + str[2];
+  if (stringBegins === "aca") {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(startsWithAca(stringTest));
+console.log(startsWithAca(stringTest2));
+
+/*E3*/
+console.log("*****E3******");
+stringTest="Hola";
+stringTest2="Adios";
+function greeting(str){
+    if(str==="Hola"){return str.repeat(3);}
+    else{return "Type Hola for greeting";}
+}
+console.log(greeting(stringTest));
+console.log(greeting(stringTest2));
