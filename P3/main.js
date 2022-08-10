3930ba3787f71ab9986187063d5b81e762484efd
Victor <vgarcialosada@gmail.com>
2022-08-10 09:54:35 +0200

    .

---
+++ P3/main.js
@@ -0,0 +1,19 @@
+var name="Victor";
+let apellido="Garcia";
+let boolean=false;
+const telf="123123123";
+const value1 =Symbol("hello");
+const human = {name: 'Victor', apellido: 'Garcia'};
+const arrayItems= ["table", "chair"];
+function helloWorld(){
+    console.log(typeof helloWorld);
+    console.log("Hello world");
+    
+}
+console.log(name);
+console.log(typeof apellido);
+console.log(typeof boolean);
+console.log(value1.description);
+console.log(human.name + " "+ human.apellido);
+console.log(arrayItems[0]);
+helloWorld();
