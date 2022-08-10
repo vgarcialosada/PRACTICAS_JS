2bf9a392a4a569d5bd7df647647a47161b25e566
Victor <vgarcialosada@gmail.com>
2022-08-10 10:18:22 +0200

    .

---
+++ P5/main.js
@@ -0,0 +1,9 @@
+let num = Math.round(Math.random());
+function headsOrTails(int) {
+  if (int == 0) {
+    return("Heads");
+  } else {
+    return("Tails");
+  }
+}
+console.log(headsOrTails(num));
